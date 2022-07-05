49c137b6964e3dffc57aa9ef3cc4205a7602ddf3
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-07-05 19:04:48 -0300

    Algumas dependências baixadas e index de teste

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Servidor online na porta: ", process.env.PORT)
+})
