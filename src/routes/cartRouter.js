4ad3216169258f3209e7f30853d8b8da20a82962
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-07-08 12:48:09 -0300

    Funcionalidades da tela carrinho prontas

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { getUserCart } from "../controllers/cartController.js";
+import { getUserCart,deleteProduct } from "../controllers/cartController.js";
 import { jtwAuth } from "../middlewares/jwtAuthMidd.js";
 
 const router = Router();
 router.get("/cart",jtwAuth, getUserCart);
-
+router.delete("/cart/:productID",jtwAuth, deleteProduct)
 export default router;
